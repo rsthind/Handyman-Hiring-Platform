87c47af7ed71a5a8c8f276baef520bb6ef066599
Ranveer Thind <ranveerthind01@hotmail.com>
2020-01-09 17:54:41 -0500

    Login Fixes

--- app/src/main/java/com/example/networkingapp/LoginActivity.java
+++ app/src/main/java/com/example/networkingapp/LoginActivity.java
@@ -1,20 +1,38 @@
 package com.example.networkingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText email, pass;
     TextView noAccount;
     Button loginBtn;
+
+    private FirebaseAuth mAuth;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +47,8 @@ public class LoginActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
+        mAuth = FirebaseAuth.getInstance();
+
         email = findViewById(R.id.email);
         pass = findViewById(R.id.password);
         noAccount = findViewById(R.id.notHave_account);
@@ -37,6 +57,14 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String emailS = email.getText().toString().trim();
+                String passS = pass.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(emailS).matches()) {
+                    email.setError("Invalid Email");
+                    email.setFocusable(true);
+                } else {
+                    loginUser(emailS, passS);
+                }
 
             }
         });
@@ -45,6 +73,37 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please Wait...");
+    }
+
+    private void loginUser(String emailS, String passS) {
+        progressDialog.show();
+        mAuth.signInWithEmailAndPassword(emailS, passS)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                            finish();
+                        } else {
+                            progressDialog.dismiss();
+
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/networkingapp/ProfileActivity.java
+++ app/src/main/java/com/example/networkingapp/ProfileActivity.java
@@ -1,10 +1,14 @@
 package com.example.networkingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,6 +16,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
+
+    TextView profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +30,15 @@ public class ProfileActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
+        profile = findViewById(R.id.profileTv);
+
     }
 
     private void checkUserStatus() {
         FirebaseUser user = firebaseAuth.getCurrentUser();
         if (user != null) {
             //user is signed in
+            profile.setText(user.getEmail());
         } else {
             //not signed in
             startActivity(new Intent(ProfileActivity.this, MainActivity.class));
@@ -41,5 +50,22 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onStart() {
         checkUserStatus();
         super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
